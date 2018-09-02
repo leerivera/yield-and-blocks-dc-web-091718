5c91214860e008b1deda4ebb2841cfecf7bbcc7c
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-02 18:13:12 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,16 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    i = 0 
+     while i < array.length 
+     yield array[i]#[] used to grab each successive index yielding #each to a block
+      i = i +1
+  end
+  array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
+
 
